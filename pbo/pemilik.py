7f82f368043d04377f1037f210b9679967daaebb
helmi3044 <56855437+helmi3044@users.noreply.github.com>
2020-12-31 22:11:21 +0700

    add pemilik.py

---
+++ pbo/pemilik.py
@@ -0,0 +1,28 @@
+import sqlite3
+from datamanager import DataManager
+from person import Person
+
+class Pemilik(Person):
+	def setDataPemilik(self, Nama, Alamat, NomorHP):
+		self.setDataPerson(Nama, Alamat, NomorHP)
+		IdPerson = self.getDataPerson(Nama, Alamat, NomorHP)
+		self.query = 'INSERT INTO Pemilik (IdPerson) \
+			values (\'%s\')' 
+		self.query = self.query % (IdPerson[0][0])
+		print('self.query : ', self.query )
+		self.executeQuery(self.query)
+		
+	def getDaftarPemilik(self):
+		self.query = 'SELECT t2.Nama, t2.Alamat, t2.NomorHP \
+			FROM Pemilik t1 \
+			join person t2 on t1.IdPerson=t2.IdPerson' 
+		# ~ print('self.query : ', self.query )
+		daftar = self.executeQuery(self.query, True)
+		return daftar
+
+	def pendapatan(self):
+		self.query = 'SELECT SUM(t2.Harga * t1.Quantity) as pendapatan \
+			FROM Pesanan t1 \
+			join Menu t2 on t1.MenuId=t2.MenuId;'
+		daftar = self.executeQuery(self.query, True)
+		return daftar
